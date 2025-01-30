490085b4971e714839066d1d543a64c7aca2e52c
José Borges <jose.carlos05062000@gmail.com>
2025-01-29 21:10:38 -0300

    Implementada a configuração de video para testes headless

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    // Habilitando a gravação dos testes
+    video: true
   },
 });
