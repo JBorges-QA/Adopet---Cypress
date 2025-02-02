d37bc0df7f7a6cb718c959846b84e63bc4e2cef6
José Borges <jose.carlos05062000@gmail.com>
2025-02-02 17:04:38 -0300

    Retirado os dados sensíveis

--- cypress/e2e/api-adopet.cy.js
+++ cypress/e2e/api-adopet.cy.js
@@ -1,11 +1,10 @@
 describe('Api Adopet', () =>{
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTczODI4MDgyOCwiZXhwIjoxNzM4NTQwMDI4fQ.KgzevvZyDzckj85NVe0Sxkfek9Rm3fOsXYU1V-WJxao`
 
     it('Mensagens da API', () =>{
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty;
@@ -17,7 +16,7 @@ describe('Api Adopet', () =>{
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: {authorization}
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty;
