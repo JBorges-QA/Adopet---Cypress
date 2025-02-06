c2f75203066faf5a02601c953df8ecc8b12ee4b5
José Borges <jose.carlos05062000@gmail.com>
2025-02-05 22:14:52 -0300

    Alterado o uso dos valores de env e implementada a verificação dos valores retornados pelo endpoint /perfil da API

--- cypress/e2e/back-end/api-adopet-perfil.cy.js
+++ cypress/e2e/back-end/api-adopet-perfil.cy.js
@@ -4,13 +4,20 @@ describe('Api Adopet - Endpoint /perfil', () =>{
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/adotante/perfil/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: Cypress.env()
+            headers: {
+                Authorization: Cypress.env("authorization"),
+            }
         }).then((res) => {
+            const bcryptRegex = /^\$2[aby]\$\d{2}\$[./A-Za-z0-9]{53}$/;
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty;
-            expect(res.body).to.have.property('perfil')
+            expect(res.body).to.have.property('perfil');
             expect(res.body.perfil).to.have.property('nome');
-            expect(res.body.perfil.nome).to.be.a('string').and.not.be.empty;
+            expect(res.body.perfil).to.have.property('email');
+            expect(res.body.perfil).to.have.property('password');
+            expect(res.body.perfil.nome).to.be.a('string').and.equal(Cypress.env("nome"));
+            expect(res.body.perfil.email).to.be.a('string').and.equal(Cypress.env("email"));
+            expect(res.body.perfil.password).to.match(bcryptRegex);
         })
     })
 
--- cypress/e2e/back-end/api-mensagens.cy.js
+++ cypress/e2e/back-end/api-mensagens.cy.js
@@ -3,7 +3,9 @@ describe('Api Adopet', () =>{
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: Cypress.env()
+            headers: {
+                Authorization: Cypress.env("authorization"),
+            }
         }).then((res) => {
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty;
