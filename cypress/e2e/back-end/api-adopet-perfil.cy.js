85b09852318c36f87238df50176db32be025c989
José Borges <jose.carlos05062000@gmail.com>
2025-02-05 21:44:15 -0300

    Refatorado o código de testes dos endpoints da API e separados em arquivos de seus respectivos endpoints

--- cypress/e2e/back-end/api-adopet-perfil.cy.js
+++ cypress/e2e/back-end/api-adopet-perfil.cy.js
@@ -1,16 +1,4 @@
-describe('Api Adopet', () =>{
-
-    it('Mensagens da API', () =>{
-        cy.request({
-            method: 'GET',
-            url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
-            headers: Cypress.env()
-        }).then((res) => {
-            expect(res.status).to.be.equal(200);
-            expect(res.body).is.not.empty;
-            expect(res.body).to.have.property('msg');
-        })
-    })
+describe('Api Adopet - Endpoint /perfil', () =>{
 
     it('Retorno do endpoint /perfil', () =>{
         cy.request({
@@ -25,4 +13,5 @@ describe('Api Adopet', () =>{
             expect(res.body.perfil.nome).to.be.a('string').and.not.be.empty;
         })
     })
+
 })
